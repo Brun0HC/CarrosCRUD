ed6d64a99f5c56092bc20eba45388022bd81af17
mauantonelli <pablojuanang@gmail.com>
2024-05-29 16:08:39 -0300

    test: verify homepage loads correctly

--- tests-selenium/src/test/java/br/all/Tests.java
+++ tests-selenium/src/test/java/br/all/Tests.java
@@ -1,4 +1,36 @@
 package br.all;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Tests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/caminho/para/chromedriver");
+
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testHomePageLoads() {
+        driver.get("https://carros-crud.vercel.app/");
+
+        assertTrue(driver.getTitle().contains("Carros"), "O título da página está correto");
+    }
 }
